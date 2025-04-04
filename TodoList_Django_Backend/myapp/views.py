e5eeaa32a527e72c92fbd2b8d71c4a7be711cabe
IyanuKwent <olandria.iankent@gmail.com>
2025-04-04 19:14:01 +0800

    yes

---
+++ TodoList_Django_Backend/myapp/views.py
@@ -0,0 +1,20 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-id')  
+    serializer_class = TaskSerializer
+
+
+    @action(detail=True, methods=['put'])
+    def toggle(self, request, pk=None):
+        try:
+            task = self.get_object() 
+            task.completed = not task.completed  
+            task.save()  
+            return Response(TaskSerializer(task).data, status=status.HTTP_200_OK)
+        except Task.DoesNotExist:
+            return Response({"detail": "Task not found."}, status=status.HTTP_404_NOT_FOUND)
