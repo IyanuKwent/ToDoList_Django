7f8257c163ddcc41566c7e15d7cfd3f67ad8020b
IyanuKwent <olandria.iankent@gmail.com>
2025-04-10 21:36:18 +0800

    changes fetch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -56,13 +56,12 @@ function App() {
     }
 
     try {
-      const response = await fetch(API_URL + "tasks/", {
+      const response = await fetch("https://todolist-django-backend.onrender.com/api-token-auth/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Token ${authToken}`,
         },
-        body: JSON.stringify({ title: task }),
+        body: JSON.stringify({ username, password }),
       });
 
       if (response.ok) {
