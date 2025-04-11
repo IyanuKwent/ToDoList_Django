954a0f416f37b812db7ba31d07f787d9cc6a3345
Ian Kent <olandria.iankent@gmail.com>
2025-04-11 08:07:40 +0800

    yes

--- TodoList_Django_Backend/myproject/urls.py
+++ TodoList_Django_Backend/myproject/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework.authtoken.views import obtain_auth_token  # ✅ NEW
+from rest_framework.authtoken.views import obtain_auth_token 
 from myapp.views import TaskViewSet
 from django.http import JsonResponse
 
@@ -20,5 +20,5 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # ✅ ADD THIS
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), 
 ]
